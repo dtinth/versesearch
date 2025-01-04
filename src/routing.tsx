4a9454d5a1ec6ef9d9214bf0fc59fe4c843e18b6
dtinth on MBP M1 <dtinth@spacet.me>
2025-01-04 22:18:23 +0700

    save last visited route

--- src/routing.tsx
+++ src/routing.tsx
@@ -9,7 +9,20 @@ window.addEventListener("hashchange", () => {
   $hash.set(location.hash);
 });
 
-export const $route = atom<[number, number]>([43, 1]);
+function getInitialRoute(): [number, number] {
+  const lastRoute = localStorage.getItem("lastRoute");
+  if (lastRoute) {
+    const parts = lastRoute.split(".");
+    const book = parseInt(parts[0]);
+    const chapter = parseInt(parts[1]);
+    if (book && chapter && verseCount[book]?.[chapter]) {
+      return [book, chapter];
+    }
+  }
+  return [43, 1];
+}
+
+export const $route = atom<[number, number]>(getInitialRoute());
 $hash.subscribe((hash) => {
   const match = hash.match(/^#(\w{3})(\d+)$/);
   if (match) {
@@ -17,6 +30,7 @@ $hash.subscribe((hash) => {
     const chapter = parseInt(match[2]);
     if (book && verseCount[book]?.[chapter]) {
       $route.set([book, chapter]);
+      localStorage.setItem("lastRoute", `${book}.${chapter}`);
     }
   }
 });
